1d33c4d0bc4b21e54488d597fad528d0ceb543d8
Dairon Medina <dairon.medina@gmail.com>
2016-02-28 03:37:29 -0500

    Added better testing

--- validator_test.go
+++ validator_test.go
@@ -1,8 +1,8 @@
-package validator_test
+package validator
 
 import(
   "testing"
-  "github.com/codeadict/ecuadorianid/validator"
+  "github.com/codeadict/ecuadorianid"
 )
 
 
@@ -10,7 +10,7 @@ func TestValid(t *testing.T) {
 	tests := []string{"1719956383"}
 
 	for _, tt := range tests {
-		if p, err := Validate(tt); err != nil {
+		if p, err := validator.Validate(tt); err != nil {
 			t.Errorf("Fail at '%v', out '%+v', err: %+v", tt, p, err)
 		}
 	}
@@ -20,7 +20,7 @@ func TestInvalid(t *testing.T) {
 	tests := []string{"1719956382"}
 
 	for _, tt := range tests {
-		if p, err := Validate(tt); err == nil {
+		if p, err := validator.Validate(tt); err == nil {
 			t.Errorf("Fail at '%v', out '%+v', should have produced error", tt, p)
 		}
 	}
